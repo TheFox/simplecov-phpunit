c2ffe98ff1724f1e8246ade3b959a78c0bf83d04
Christian Mayer <christian@fox21.at>
2016-09-21 13:59:48 +0200

    Use CDNs to load jQuery and Bootstrap. #RN
    Removed local assets. Use remote JavaScript and CSS files instead.
    
    - http://code.jquery.com/jquery-1.12.4.min.js
    - https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css

--- lib/simplecov-phpunit/version.rb
+++ lib/simplecov-phpunit/version.rb
@@ -4,8 +4,8 @@ module SimpleCov
 		
 		module PHPUnitFormatter
 			
-			VERSION = '0.3.0'
-			DATE = '2016-09-19'
+			VERSION = '0.4.0-dev'
+			DATE = '2016-09-21'
 			HOMEPAGE = 'https://github.com/TheFox/simplecov-phpunit'
 			
 		end
