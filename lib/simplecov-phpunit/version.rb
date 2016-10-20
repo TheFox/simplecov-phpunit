f53d59b4e5fe013dfe2f1b83ab177f223f9e8a8d
Christian Mayer <christian@fox21.at>
2016-10-20 16:14:05 +0200

    Test only on tags.

--- lib/simplecov-phpunit/version.rb
+++ lib/simplecov-phpunit/version.rb
@@ -4,8 +4,8 @@ module SimpleCov
 		
 		module PHPUnitFormatter
 			
-			VERSION = '0.4.2'
-			DATE = '2016-10-04'
+			VERSION = '1.0.0-dev'
+			DATE = '2016-10-20'
 			HOMEPAGE = 'https://github.com/TheFox/simplecov-phpunit'
 			
 		end
